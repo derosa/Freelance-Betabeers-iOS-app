7f659740bb6b4b43efdb8bc1b48b4fe5d546f5de
StaS <stas@stasbandol.com>
2012-06-24 00:36:02 +0200

    - calculate cells height based on job title in JobTableController - added some TODOs - some code reordering for easier reading

--- Freelance/JobTableController.m
+++ Freelance/JobTableController.m
@@ -18,6 +18,101 @@
 
 @synthesize tableView;
 
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+
+
+- (void)viewDidAppear:(BOOL)animated{
+    
+    [super viewDidAppear:animated];
+    
+    //TODO: check connection only on request fail, it's not a flight ticket :)
+    if ([self connectedToNetwork] ) {
+        
+        [SVProgressHUD show];
+        
+        NSString *url = @"http://migueldev.com/freelance/trabajos.php";
+        //TODO: should be done asynchronously
+        NSData *items = [NSData dataWithContentsOfURL:[NSURL URLWithString:url]];
+        NSInputStream *stream = [[NSInputStream alloc] initWithData:items];
+        [stream open];
+        
+        if (stream) {
+            
+            arrayC = [[NSMutableArray alloc] init];
+            
+            NSError *parseError = nil;
+            id jsonObject = [NSJSONSerialization JSONObjectWithStream:stream options:NSJSONReadingAllowFragments error:&parseError];
+            
+            NSArray *items = [[jsonObject objectForKey:@"response"] objectForKey:@"jobs"];
+            [items enumerateObjectsUsingBlock:^(NSDictionary *item, NSUInteger idx, BOOL *stop) {
+                [arrayC addObject:item];
+            }];
+            
+            [self.tableView reloadData]; 
+            
+        } else {
+            NSLog(@"Failed to open stream.");
+        }
+        
+        [SVProgressHUD dismiss];
+        
+        
+    }else{
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle: @"Error" message: @"Hace falta conexión a internet" delegate: self cancelButtonTitle: @"Cancelar" otherButtonTitles: @"Reintentar", nil];
+        [alert show];
+    }
+    
+}
+
+
+- (void)viewDidDisappear:(BOOL)animated{
+    
+    [super viewDidDisappear:animated];
+    [SVProgressHUD dismiss];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+
+
+#pragma mark -
+#pragma mark Custom Methods
+
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([[segue identifier] isEqualToString:@"getJob"]) 
+    {
+        JobViewController *destination = [segue destinationViewController];
+        NSIndexPath * indexPath = (NSIndexPath*)sender;
+        
+        destination.job = [arrayC objectAtIndex:[indexPath row]];
+    }
+}
+
+
+
+- (void)alertView:(UIAlertView *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (buttonIndex != 0){
+        [self viewDidAppear:YES];
+    }
+}
+
 ////////////////////////
 
 
@@ -50,111 +145,43 @@
 
 ////////////////////////
 
-- (void)viewDidLoad
+
+#pragma mark -
+#pragma mark IBActions
+
+
+- (IBAction)info:(id)sender
 {
-    [super viewDidLoad];
-
-    
-    connected = FALSE;
+    UIAlertView *someError = [[UIAlertView alloc] initWithTitle: @"Información" message: @"Las ofertas de empleo se dan de alta en http://dir.betabeers.com" delegate: self cancelButtonTitle: @"Ok" otherButtonTitles: nil];
+    [someError show];
 }
-
-
-
-
-- (void)viewDidAppear:(BOOL)animated{
-    
-    [super viewDidAppear:animated];
-
-    if ( connected == FALSE ) {
-        
-        connected = [self connectedToNetwork];
-
-        if( connected ){
-            
-            
-            [SVProgressHUD show];
-            
-            NSString *url = @"http://migueldev.com/freelance/trabajos.php";
-       
-         
-            // fill table
-            arrayC = [[NSMutableArray alloc] init];
-
-            
-            
-            NSData *items = [NSData dataWithContentsOfURL:[NSURL URLWithString:url]];
-            NSInputStream *stream = [[NSInputStream alloc] initWithData:items];
-            [stream open];
-            
-            if (stream) {
-                
-                
-            
-                NSError *parseError = nil;
-                id jsonObject = [NSJSONSerialization JSONObjectWithStream:stream options:NSJSONReadingAllowFragments error:&parseError];
-                
-                
-                NSArray *items = [[jsonObject objectForKey:@"response"] objectForKey:@"jobs"];
-                for (NSDictionary *item in items) {
-                    
-                    
-                    
-                    NSLog(@"%@",[item objectForKey:@"title"]);
-                    
-                    
-                    
-                    [arrayC addObject:item];
-
-                    
-                }
-                
-                [tableView reloadData]; 
-                
-                
-                [SVProgressHUD dismiss];
-
-            } else {
-                NSLog(@"Failed to open stream.");
-            }
-        
-        
-        }else{
-            UIAlertView *alert = [[UIAlertView alloc] initWithTitle: @"Error" message: @"Hace falta conexión a internet" delegate: self cancelButtonTitle: @"Cancelar" otherButtonTitles: @"Reintentar", nil];
-            [alert show];
-        }
-            
-            
-    }
-
-}
-
-
-- (void)viewDidDisappear:(BOOL)animated{
-    
-    [super viewDidDisappear:animated];
-    [SVProgressHUD dismiss];
-}
-
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-}
-
 
 
 //////////////
+
+#pragma mark -
+#pragma mark UITableViewdataSource & UITableViewDelegate
 
 
 // tabla
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return[arrayC count];
 }
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    static const CGFloat kCellMinHeight = 44.f;
+    static const CGFloat kJobTitleLabelFontSize = 20.f;
+    static const CGFloat kJobTitleWidth = 300.f;
+    static const CGFloat kJobTitleVerticalMargin = 10.f;
+    
+    NSString *jobTitle = [[arrayC objectAtIndex:indexPath.row] objectForKey:@"title"];
+    CGSize size = [jobTitle sizeWithFont:[UIFont boldSystemFontOfSize:kJobTitleLabelFontSize] constrainedToSize:CGSizeMake(kJobTitleWidth, CGFLOAT_MAX)];
+    
+    return MAX(kCellMinHeight, size.height + kJobTitleVerticalMargin);
+}
+
+
 - (UITableViewCell *)tableView:(UITableView *)tabla cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
     static NSString *CellIdentifier = @"Cell";
@@ -162,10 +189,12 @@
     UITableViewCell *cell = [tabla dequeueReusableCellWithIdentifier:CellIdentifier];
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        cell.textLabel.numberOfLines = 0;
     }
     
 	NSString *cellValue =[[arrayC objectAtIndex:indexPath.row] objectForKey:@"title"];
 	cell.textLabel.text = cellValue ;
+    
     
     return cell;
 }
@@ -176,31 +205,5 @@
 }
 
 
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    if ([[segue identifier] isEqualToString:@"getJob"]) 
-    {
-        JobViewController *destination = [segue destinationViewController];
-        NSIndexPath * indexPath = (NSIndexPath*)sender;
-        
-
-        
-        destination.job = [arrayC objectAtIndex:[indexPath row]];
-    }
-}
-
-
-
-- (void)alertView:(UIAlertView *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
-    if (buttonIndex != 0){
-        [self viewDidAppear:YES];
-    }
-}
-
-- (IBAction)info:(id)sender
-{
-    UIAlertView *someError = [[UIAlertView alloc] initWithTitle: @"Información" message: @"Las ofertas de empleo se dan de alta en http://dir.betabeers.com" delegate: self cancelButtonTitle: @"Ok" otherButtonTitles: nil];
-    [someError show];
-}
 
 @end
